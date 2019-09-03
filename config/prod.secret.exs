3d195c1e9fe8bfcf30655f3e95cdfe1dee88db5f
kaashyapan <sunder.narayanaswamy@gmail.com>
2019-09-03 13:04:12 +0530

    Initial commit

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,22 +5,8 @@
 use Mix.Config
 
 secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+  "wA+tO6BGOylR0EBXT3CyxVCc0jUJ4IcKucHswZzzRXrqOW4dJb6NFg8GXu+BknB8"
 
 config :apigateway, ApigatewayWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :apigateway, ApigatewayWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
